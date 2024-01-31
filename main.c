0c3cc2f16326ec32719480b4875e5e179e97d6f0
lvcky-gg <matadorcrab@gmail.com>
2024-01-31 16:39:18 -0500

    compiles

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
+#include "helpers/vector.h"
+#include "compiler.h"
 int main()
 {
-    printf("hello world\n");
+    int res = compile_file("./test.c", "./test", 0);
+    if (res == COMPILER_FILE_COMPILED_OK)
+    {
+        printf("Good to go!\n");
+    }
+    else if (res == COMPILER_FAILED_WITH_ERRORS)
+    {
+        printf("you write bad code\n");
+    }
+    else
+    {
+        printf("WHAT HAVE YOU DONE???? Unknown response\n");
+    }
     return 0;
 }
