12b909b32ec6b4345d9277fd347c8a6038ed9c9e
lvcky-gg <matadorcrab@gmail.com>
2024-02-01 14:55:38 -0500

    init lex

--- compiler.h
+++ compiler.h
@@ -44,8 +44,13 @@ struct token
 struct lex_process;
 
 typedef char (*LEX_PROCESS_NEXT_CHAR)(struct lex_process *process);
+typedef char (*LEX_PROCESS_PEEK_CHAR)(struct lex_process *process);
+typedef void (*LEX_PROCESS_PUSH_CHAR)(struct lex_process *process, char c);
 struct lex_process_functions
 {
+  LEX_PROCESS_NEXT_CHAR next_char;
+  LEX_PROCESS_PEEK_CHAR peek_char;
+  LEX_PROCESS_PUSH_CHAR push_char;
 };
 
 struct lex_process
@@ -71,6 +76,8 @@ struct compile_process
 {
   int flags;
 
+  struct pos pos;
+
   struct compile_process_input_file
   {
     FILE *fp;
@@ -83,5 +90,9 @@ int compile_file(const char *filename, const char *out_filename, int flags);
 struct compile_process *compile_process_create(const char *filename,
                                                const char *filename_out,
                                                int flags);
+
+char compile_process_next_char(struct lex_process *lex_process);
+char compile_process_peek_char(struct lex_process *lex_process);
+void compile_process_push_char(struct lex_process *lex_process, char c);
 
 #endif
--- cprocess.c
+++ cprocess.c
@@ -3,15 +3,19 @@
 #include <stdlib.h>
 struct compile_process *compile_process_create(const char *filename,
                                                const char *filename_out,
-                                               int flags) {
+                                               int flags)
+{
   FILE *file = fopen(filename, "r");
-  if (!file) {
+  if (!file)
+  {
     return NULL;
   }
   FILE *out_file = NULL;
-  if (filename_out) {
+  if (filename_out)
+  {
     out_file = fopen(filename_out, "w");
-    if (!out_file) {
+    if (!out_file)
+    {
       return NULL;
     }
   }
@@ -20,4 +24,31 @@ struct compile_process *compile_process_create(const char *filename,
   process->cfile.fp = file;
   process->ofile = out_file;
   return process;
+}
+
+char compile_process_next_char(struct lex_process *lex_process)
+{
+  struct compile_process *compiler = lex_process->compiler;
+  compiler->pos.col += 1;
+  char c = getc(compiler->cfile.fp);
+  if (c == "\n")
+  {
+    compiler->pos.line += 1;
+    compiler->pos.col = 1;
+  }
+  return c;
+}
+
+char compile_process_peek_char(struct lex_process *lex_process)
+{
+  struct compile_process *compiler = lex_process->compiler;
+  char c = getc(compiler->cfile.fp);
+  ungetc(c, compiler->cfile.fp);
+  return c;
+}
+
+void compile_process_push_char(struct lex_process *lex_process, char c)
+{
+  struct compile_process *compiler = lex_process->compiler;
+  ungetc(c, compiler->cfile.fp);
 }
