028b8da5da29526377c73c9ecff02091ef648a97
lvcky-gg <matadorcrab@gmail.com>
2024-02-01 17:58:28 -0500

    error handle

--- lexxer.c
+++ lexxer.c
@@ -1,5 +1,43 @@
 #include "compiler.h"
+#include "helpers/vector.h"
+static struct lex_process *lex_process;
+
+static char peekc()
+{
+    return lex_process->function->peek_char(lex_process);
+}
+static void pushc(char c)
+{
+    lex_process->function->push_char(lex_process, c);
+}
+
+struct token *read_next_token()
+{
+    struct token *token = NULL;
+
+    return token;
+}
+
 int lex(struct lex_process *process)
 {
-    return 0;
+    process->current_expression_count = 0;
+    process->parentheses_buffer = NULL;
+    lex_process = process;
+    process->pos.filename = process->compiler->cfile.abs_path;
+
+    struct token *token = read_next_token();
+    while (token)
+    {
+        vector_push(process->token_vec, token);
+        char c = peekc();
+        switch (c)
+        {
+        case EOF:
+            break;
+        default:
+            compiler_error(lex_process->compiler, "Unexpected Token\n");
+        }
+        token = read_next_token();
+    }
+    return LEXICAL_ANALYSIS_ALL_OK;
 }
